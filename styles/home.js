aafb70770f349767d394b0d33daa4e5c69123550
Roberto <robertomeneses12513321@gmail.com>
2024-09-18 17:23:36 -0300

    Ajeitando o merge

--- styles/home.js
+++ styles/home.js
@@ -1,88 +1,88 @@
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingLeft: 10,
-      paddingRight: 10,
-      backgroundColor: '#fff',
-    },
-    searchBar: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 2,
-      marginBottom: 10,
-      paddingHorizontal: 8,
-    },
-    meuMapa: {
-      height: 178,
-      width: width * 0.9,
-      alignSelf: 'center',
-      marginTop: 10,
-      resizeMode: 'contain',
-    },
-    list: {
-      flexGrow: 1,
-      marginVertical: 10,
-    },
-    reactLogo: {
-      height: 178,
-      width: 290,
-      alignSelf: 'center',
-      marginBottom: 10,
-    },
-    listItem: {
-      padding: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#000000',
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      marginBottom: 10, 
-    },
-    title: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    subtitle: {
-      fontSize: 16,
-      marginVertical: 5,
-    },
-    detailsButton: {
-      backgroundColor: '#3498db',
-      paddingVertical: 6,
-      paddingHorizontal: 10,
-      borderRadius: 5,
-      marginRight: 10, 
-    },
-    detailsButtonText: {
-      color: '#fff',
-      fontSize: 14,
-      fontWeight: 'bold',
-    },
-    routeButton: {
-      backgroundColor: '#2ecc71',
-      paddingVertical: 6,
-      paddingHorizontal: 10,
-      borderRadius: 5,
-    },
-    routeButtonText: {
-      color: '#fff',
-      fontSize: 14,
-      fontWeight: 'bold',
-    },
-    buttonContainer: {
-      flexDirection: 'row',
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-    },
-    emptyMessage: {
-      textAlign: 'center',
-      fontSize: 16,
-      color: 'gray',
-    },
-    footerMessage: {
-      textAlign: 'center',
-      fontSize: 16,
-      color: 'gray',
-    },
-  });
-  
-  export default styles;
+  container: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: '#fff',
+  },
+  searchBar: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 2,
+    marginBottom: 10,
+    paddingHorizontal: 8,
+  },
+  meuMapa: {
+    height: 178,
+    width: width * 0.9,
+    alignSelf: 'center',
+    marginTop: 10,
+    resizeMode: 'contain',
+  },
+  list: {
+    flexGrow: 1,
+    marginVertical: 10,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    alignSelf: 'center',
+    marginBottom: 10,
+  },
+  listItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000000',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+    marginVertical: 5,
+  },
+  detailsButton: {
+    backgroundColor: '#3498db',
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  detailsButtonText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  routeButton: {
+    backgroundColor: '#2ecc71',
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  routeButtonText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  emptyMessage: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'gray',
+  },
+  footerMessage: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'gray',
+  },
+});
+
+export default styles;
